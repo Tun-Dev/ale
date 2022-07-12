51f072cda6d182f519f1fd60a8333e75188454b3
Douye Tungbulu <tungbulupaul@gmail.com>
2022-07-12 17:41:00 +0100

    Slight changes

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -11311,129 +11311,98 @@ function contentAnimation() {
   });
 
   if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
-    // loderTl.from(".load-bar", { delay: 0.5, opacity: 0, duration: 1 });
-    // loderTl
-    //   .to(".word1", { duration: 0.7, y: "0%" })
-    //   .to(".word1", {
-    //     delay: 0.3,
-    //     duration: 0.5,
-    //     opacity: 0,
-    //   })
-    //   .to(".word2", { duration: 0.7, y: "0%" })
-    //   .to(".word2", {
-    //     delay: 0.7,
-    //     duration: 0.5,
-    //     opacity: 0,
-    //   })
-    //   .to(".word3", { duration: 0.7, y: "0%" })
-    //   .to(".word3", {
-    //     delay: 0.7,
-    //     duration: 0.5,
-    //     opacity: 0,
-    //   })
-    //   .to(".word4", { duration: 0.7, y: "0%" })
-    //   .to(".word4", {
-    //     delay: 0.7,
-    //     duration: 0.5,
-    //     opacity: 0,
-    //   })
-    //   .to(".word5", { duration: 0.7, y: "0%" })
-    //   .to(".word5", {
-    //     delay: 0.5,
-    //     duration: 0.5,
-    //     opacity: 0,
-    //   })
-    //   .to(".word6", { duration: 0.7, y: "0%" });
-    // loderTl
-    //   .to(
-    //     ".bar",
-    //     {
-    //       width: "30%",
-    //       duration: 6,
-    //       ease: "power4.out",
-    //     },
-    //     "-=9"
-    //   )
-    //   .to(
-    //     ".bar",
-    //     {
-    //       width: "70%",
-    //       duration: 6,
-    //       ease: "power4.out",
-    //     },
-    //     "-=7"
-    //   )
-    //   .to(
-    //     ".bar",
-    //     {
-    //       width: "100%",
-    //       duration: 6,
-    //       ease: "power4.out",
-    //     },
-    //     "-=4"
-    //   );
-    // loderTl.to([".load-words", ".load-bar"], { delay: 0, opacity: 0 }).to(
-    //   ".home-loader",
-    //   {
-    //     delay: 0.9,
-    //     opacity: 0,
-    //     height: 0,
-    //     duration: 1.5,
-    //     ease: "Expo.easeInOut",
-    //   },
-    //   "-=1"
-    // );
-    // tl.to(".imgcon-inner .after", {
-    //   delay: 1,
-    //   duration: 1.5,
-    //   height: "0%",
-    //   ease: "power2.out",
-    // });
-    // tl.from(
-    //   ".imgcon-inner img",
-    //   {
-    //     duration: 1.4,
-    //     scale: 1.6,
-    //     ease: "Power2.easeInOut",
-    //   },
-    //   "-=1.5"
-    // );
-    // tl.to(".name h1", { duration: 0.9, y: "0%", delay: 0.7 }, "-=2");
-    // tl.to(".title h4", {
-    //   duration: 0.5,
-    //   y: "0%",
-    //   stagger: {
-    //     amount: 0.5,
-    //   },
-    // });
-    // tl.to(
-    //   ".line",
-    //   {
-    //     duration: 0.5,
-    //     y: "0%",
-    //     stagger: {
-    //       amount: 0.5,
-    //     },
-    //   },
-    //   "-=.5"
-    // );
-    // tl.to(
-    //   ".links li",
-    //   {
-    //     duration: 0.5,
-    //     y: "0%",
-    //     stagger: {
-    //       amount: 0.5,
-    //     },
-    //   },
-    //   "-=.5"
-    // );
+    loderTl.from(".load-bar", {
+      delay: 0.5,
+      opacity: 0,
+      duration: 1
+    });
+    loderTl.to(".word1", {
+      duration: 0.7,
+      y: "0%"
+    }).to(".word1", {
+      delay: 0.3,
+      duration: 0.5,
+      opacity: 0
+    }).to(".word2", {
+      duration: 0.7,
+      y: "0%"
+    }).to(".word2", {
+      delay: 0.7,
+      duration: 0.5,
+      opacity: 0
+    }).to(".word3", {
+      duration: 0.7,
+      y: "0%"
+    }).to(".word3", {
+      delay: 0.7,
+      duration: 0.5,
+      opacity: 0
+    }).to(".word4", {
+      duration: 0.7,
+      y: "0%"
+    }).to(".word4", {
+      delay: 0.7,
+      duration: 0.5,
+      opacity: 0
+    }).to(".word5", {
+      duration: 0.7,
+      y: "0%"
+    }).to(".word5", {
+      delay: 0.5,
+      duration: 0.5,
+      opacity: 0
+    }).to(".word6", {
+      duration: 0.7,
+      y: "0%"
+    });
+    loderTl.to(".bar", {
+      width: "30%",
+      duration: 6,
+      ease: "power4.out"
+    }, "-=9").to(".bar", {
+      width: "70%",
+      duration: 6,
+      ease: "power4.out"
+    }, "-=7").to(".bar", {
+      width: "100%",
+      duration: 6,
+      ease: "power4.out"
+    }, "-=4");
+    loderTl.to([".load-words", ".load-bar"], {
+      delay: 0,
+      opacity: 0
+    }).to(".home-loader", {
+      delay: 0.9,
+      opacity: 0,
+      height: 0,
+      duration: 1.5,
+      ease: "Expo.easeInOut"
+    }, "-=1");
     tl.from(".shuffle-con", {
       duration: 2,
       opacity: 0,
       ease: "power4.in" // height: "365px",
 
     });
+    tl.to(".name h1", {
+      duration: 0.9,
+      y: "0%",
+      delay: 0.7
+    });
+    tl.to(".title h4", {
+      duration: 0.5,
+      y: "0%",
+      stagger: {
+        amount: 0.5
+      }
+    });
+    tl.to(".links li", {
+      duration: 0.5,
+      y: "0%",
+      stagger: {
+        amount: 0.5
+      }
+    }, "-=.5");
     tl.to(".shuffle-con > .block", {
       duration: 2,
       // y: "0%",
