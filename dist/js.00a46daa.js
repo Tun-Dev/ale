190037b493b2865120890af16f989224f35d3b38
Douye Tungbulu <tungbulupaul@gmail.com>
2022-08-01 16:08:32 +0100

    scroll fix

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -11725,7 +11725,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41823" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40225" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
